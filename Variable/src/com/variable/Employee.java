f0bbd4d39ed7f6262f589d12c93509e07f29022e
raushanraj07 <raushanraj83.rr@gmail.com>
2022-02-23 10:36:52 +0530

    adding java project

---
+++ Variable/src/com/variable/Employee.java
@@ -0,0 +1,23 @@
+package com.variable;
+
+public class Employee {
+	
+	int eid;
+	String ename;
+	double salary;
+	
+	public Employee(){
+		
+	}
+
+	public Employee(int eid, String ename, double salary) {
+		super();
+		this.eid = eid;
+		this.ename = ename;
+		this.salary = salary;
+	}
+
+
+	
+
+}
---
+++ mynewproject/src/project/Demo.java
@@ -0,0 +1,9 @@
+package project;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	System.out.println("welcome");	
+
+	}
+}
