4e118db09315312ce38b9d6ec2937f4fa93bf9e4
raushanraj07 <99873893+raushanraj07@users.noreply.github.com>
2022-04-12 11:31:19 +0530

    Delete phase 3/assessment project/Ecommerce Sporty Shoes/practise project/UserFeedback directory

--- phase 3/assessment project/Ecommerce Sporty Shoes/practise project/UserFeedback/src/main/java/com/example/UserFeedback/repositories/FeedbackRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.UserFeedback.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.UserFeedback.entities.*;
-
-public interface FeedbackRepository extends CrudRepository<Feedback, Integer>{
-
-}
